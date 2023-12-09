9e67679b9b89faabc5092ab2f90edd8e0af960d6
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-09 13:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+const rowNum = 20;
+const colNum = 40;
+let container = document.querySelector(".container");
+
+for (let i = 0; i < rowNum; i++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+
+  for (let j = 0; j < colNum; j++) {
+    let col = document.createElement("div");
+    col.classList.add("square");
+    col.classList.add("disappear");
+    row.appendChild(col);
+  }
+  container.appendChild(row);
+}
+
+function generateRandomNumber() {
+  return 0 + Math.floor((255 - 0) * Math.random());
+}
+
+function generateRandomColor() {
+  let randomColor = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`;
+  return randomColor;
+}
+
+let allCols = document.querySelectorAll(".square");
+
+allCols.forEach((col) => {
+  col.addEventListener("mouseover", () => {
+    col.style.backgroundColor = generateRandomColor();
+  });
+
+  col.addEventListener("mouseout", () => {
+    setTimeout(() => {
+      col.style.backgroundColor = "transparent";
+    }, 1000);
+  });
+});
